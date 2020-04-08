34d47c8e7509a08a22d3ad3cedc472d1b65ec4b4
Krzysztof Surowiec <krzysztof.surowiec@hotmail.com>
2020-04-08 11:59:20 +0200

    cleaning

--- Classes/electric_car.py
+++
@@ -1,8 +0,0 @@
-from battery import Battery
-from car import Car
-
-
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery = Battery()
