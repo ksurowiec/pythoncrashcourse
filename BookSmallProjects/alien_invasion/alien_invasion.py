5b8752d6b70678b3bd35fbb9fd0323aabe7432de
Krzysztof Surowiec <krzysztof.surowiec@hotmail.com>
2020-04-14 20:07:11 +0200

    Add space ship to canvas, refactor the code.

--- BookSmallProjects/alien_invasion/alien_invasion.py
+++ BookSmallProjects/alien_invasion/alien_invasion.py
@@ -1,6 +1,8 @@
-import sys
 import pygame
+
+import game_functions as gf
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -13,19 +15,13 @@ def run_game():
     # Set the background color
     bg_color = (230, 230, 230)
 
+    # Make a ship
+    ship = Ship(screen)
 
     # Start the main loop
     while True:
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
