4b2433e1a92d91ecf796c0e8138bfbae3c36acb3
Krzysztof Surowiec <krzysztof.surowiec@hotmail.com>
2020-04-14 18:38:10 +0200

    Alien Invasion first commit.

---
+++ BookSmallProjects/alien_invasion/settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize game settings"""
+
+        # General settings
+        self.game_title = "Alien Invasion"
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
