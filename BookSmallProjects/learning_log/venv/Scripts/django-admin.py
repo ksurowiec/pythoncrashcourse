c26603cf666f6deae315030df29b997683864a67
Krzysztof Surowiec <krzysztof.surowiec@hotmail.com>
2020-05-11 13:24:31 +0200

    Basic structure for topics and entries.

---
+++ BookSmallProjects/learning_log/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\krzysztof\source\repos\pythoncrashcourse\booksmallprojects\learning_log\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
